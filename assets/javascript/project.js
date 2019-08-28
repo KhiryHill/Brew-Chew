350f45a7f87b5ad3da65f13841ec03d673954b67
Denise Middleton <demiddleton@bellsouth.net>
2019-08-28 19:55:42 -0400

    initial commit

---
+++ assets/javascript/project.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+//Create a function to hide the start button when it is clicked
+$("#startButton").click(function () {
+    $(".start").hide();
+    $(".questions").show();
+}

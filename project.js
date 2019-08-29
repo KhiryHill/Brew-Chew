268b02609bda6d338fb27879201f2c12ded38918
Jim Chung <jchung225@gmail.com>
2019-08-28 20:49:41 -0400

    javascript update

---
+++ project.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //Create a function to hide the start button when it is clicked
+    $("#yesButton").on("click", function(){
+        $("#content-row").hide();
+        $(".questions").show();
+    })
+    var zipCodeBox = $("#zip-code");
+    var errorDiv = container.find("div.text-error");
+    //Response Handler
+    function theResponse(data){
+    }
+ });

ad5f44d58d9cfa639651285155d6e26bc08e956b
alex rhone <arhone4259@gmail.com>
2019-08-28 21:09:45 -0400

    fixed links

--- project.js
+++ project.js
@@ -9,7 +9,6 @@ $(document).ready(function () {
     //Response Handler
     function theResponse(data){
     }
-<<<<<<< HEAD
 
     var clientKey = "js-9qZHzu2Flc59Eq5rx10JdKERovBlJp3TQ3ApyC4TOa3tA8U7aVRnFwf41RpLgtE7";
 		
@@ -79,74 +78,3 @@ $(document).ready(function () {
  });
  
  
-=======
- });
-
- $(function() {
-    // IMPORTANT: Fill in your client key
-    var clientKey = "js-9qZHzu2Flc59Eq5rx10JdKERovBlJp3TQ3ApyC4TOa3tA8U7aVRnFwf41RpLgtE7";
-    
-    var cache = {};
-    var container = $("#example1");
-    var errorDiv = container.find("div.text-error");
-    
-    /** Handle successful response */
-    function handleResp(data)
-    {
-        // Check for error
-        if (data.error_msg)
-            errorDiv.text(data.error_msg);
-        else if ("city" in data)
-        {
-            // Set city and state
-            container.find("input[name='city']").val(data.city);
-            container.find("input[name='state']").val(data.state);
-        }
-    }
-    
-    // Set up event handlers
-    container.find("input[name='zipcode']").on("keyup change", function() {
-        // Get zip code
-        var zipcode = $(this).val().substring(0, 5);
-        if (zipcode.length == 5 && /^[0-9]+$/.test(zipcode))
-        {
-            // Clear error
-            errorDiv.empty();
-            
-            // Check cache
-            if (zipcode in cache)
-            {
-                handleResp(cache[zipcode]);
-            }
-            else
-            {
-                // Build url
-                var url = "https://www.zipcodeapi.com/rest/"+clientKey+"/info.json/" + zipcode + "/radians";
-                
-                // Make AJAX request
-                $.ajax({
-                    "url": url,
-                    "dataType": "json"
-                }).done(function(data) {
-                    handleResp(data);
-                    
-                    // Store in cache
-                    cache[zipcode] = data;
-                }).fail(function(data) {
-                    if (data.responseText && (json = $.parseJSON(data.responseText)))
-                    {
-                        // Store in cache
-                        cache[zipcode] = json;
-                        
-                        // Check for error
-                        if (json.error_msg)
-                            errorDiv.text(json.error_msg);
-                    }
-                    else
-                        errorDiv.text('Request failed.');
-                });
-            }
-        }
-    }).trigger("change");
-});
->>>>>>> dc9cf90d82e9e15c2720394d8928fee1cb348fd9

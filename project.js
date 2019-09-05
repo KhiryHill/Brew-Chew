3092309e5530cc47ccef72e669c751d40b375928
Khiry Hill <khiryhill7@gmail.com>
2019-09-04 21:14:17 -0400

    jquery shows console log bars

--- project.js
+++ project.js
@@ -1,12 +1,32 @@
 $(document).ready(function () {
+
+
+var name = "";
+
+    var barURL = "https://api.openbrewerydb.org/breweries?by_state=north_carolina";
+
+    $.ajax({
+        url: barURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log("Length of response: " + response.length)
+        for (var i = 0; i < response.length; i++) {
+         name = response[i].name;
+         console.log(name);
+        }
+    });
+
+    var barList = $("<th>").text(name);
+
+    $(".table").append(barList);
+    
+
     //Create a function to hide the start button when it is clicked
-    $("#yesButton").on("click", function(){
+    $("#yesButton").on("click", function () {
         $("#content-row").hide();
         $(".questions").show();
-    })
-    var zipCodeBox = $("#zip-code");
-    var errorDiv = container.find("div.text-error");
-    //Response Handler
-    function theResponse(data){
-    }
- });
+    });
+
+    
+});

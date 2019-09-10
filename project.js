d072fe04ae4f51e1bc1c7dff810fa11ee041351f
Jim Chung <jchung225@gmail.com>
2019-09-09 20:45:47 -0400

    merged all group files into 1

--- project.js
+++ project.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
 
 var name = "";
-var location = {};
     var barURL = "https://api.openbrewerydb.org/breweries?by_state=north_carolina";
 
     $.ajax({
@@ -21,33 +20,66 @@ var location = {};
             $("#url").append(website + "<br>");
         }
     });
+
+
+
     //Create a function to hide the start button when it is clicked
     $("#yesButton").on("click", function () {
         $("#content-row").hide();
         $(".questions").show();
     });
 
-    var resturantsApi ="https://developers.zomato.com/api/v2.1/search?entity_id=303&entity_type=city&q=Resturants&lat=35.3097604&lon=-80.7493377"
-
-    $.ajax({
-        url:resturantsApi,
-        method:"GET",
-    }).then(function(eatApi){
-        console.log(eatApi);
-    
-
-        for( var i=0; i < eatApi.length; i++){
-
-            var 
-            name= eatApi[i].name;
-            console.log(name);
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/28213",
+        "method": "GET",
+        "headers": {
+          "X-RapidAPI-Host": "us-restaurant-menus.p.rapidapi.com",
+          "X-RapidAPI-Key": "a2a146e526msh7075b7250515c75p13d74bjsn4414590d1dc6"
+        }
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        for (var i= 0; i <response.result.data.length; i++){
+            address = response.result.data[i].address.formatted;
+            ResName = response.result.data[i].restaurant_name;
+            PhoneNumber = response.result.data[i].restaurant_phone;
+            Cuisines = response.result.data[i].cuisines;
+            
+            console.log(ResName);
+            console.log(PhoneNumber);
+            console.log(address);
+            console.log(Cuisines)
         }
 
-
-    })
-
+      });
 
 
+
+      var menuitems = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://us-restaurant-menus.p.rapidapi.com/restaurant/47773/menuitems?page=3",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
+            "x-rapidapi-key": "a2a146e526msh7075b7250515c75p13d74bjsn4414590d1dc6"
+        }
+    }
+    
+    $.ajax(menuitems).done(function (response) {
+        console.log(response);
+
+        for ( var i=0; i <response.result.data.length; i++){
+            menuitemname = response.result.data[i].menu_item_name;
+            menuitem = response.result.data[i].menu_item_pricing;
+
+            console.log(menuitemname);
+            console.log(menuitem);
+        }
+    });
 
 });
 

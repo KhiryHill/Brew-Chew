2bfc64f9c2d19e34789e82be6e422cf64ca6a291
Jim Chung <jchung225@gmail.com>
2019-09-09 20:38:32 -0400

    merging two files

--- project.js
+++ project.js
@@ -2,45 +2,52 @@ $(document).ready(function () {
 
 
 var name = "";
-
+var location = {};
     var barURL = "https://api.openbrewerydb.org/breweries?by_state=north_carolina";
 
     $.ajax({
         url: barURL,
-        method: "GET"
+        method: "GET",
     }).then(function (response) {
         console.log(response);
-        console.log("Length of response: " + response.length)
         for (var i = 0; i < response.length; i++) {
-		fullInfo=response[i];
-		 name = response[i].name;
-		 state=response[i].state;
-		 city=response[i].city;
-		 street= response[i].street;
-		 postal= response[i].postal_code;
-		 website=response[i].website_url;
-
-		 location= street,state,city,postal
-
-		console.log(fullInfo);
-		 console.log(name);
-		 console.log(location);
-		 console.log(website);
-		 
+            name = response[i].name;
+            street = response[i].street;
+            phone = response[i].phone;
+            website = response[i].website_url;
+            $('#bar-name').append(name + "<br>");
+            $("#location").append(street + "<br>");
+            $("#phone").append(phone + "<br>");
+            $("#url").append(website + "<br>");
         }
     });
-
-    
-
-	$(".table").append(name + "<br>" + response);
-	
-    
-
     //Create a function to hide the start button when it is clicked
     $("#yesButton").on("click", function () {
         $("#content-row").hide();
         $(".questions").show();
     });
 
+    var resturantsApi ="https://developers.zomato.com/api/v2.1/search?entity_id=303&entity_type=city&q=Resturants&lat=35.3097604&lon=-80.7493377"
+
+    $.ajax({
+        url:resturantsApi,
+        method:"GET",
+    }).then(function(eatApi){
+        console.log(eatApi);
     
+
+        for( var i=0; i < eatApi.length; i++){
+
+            var 
+            name= eatApi[i].name;
+            console.log(name);
+        }
+
+
+    })
+
+
+
+
 });
+
